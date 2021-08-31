0ad1df50d0df630e774095482a2399972e3c730d
Natai <liuleicumt@163.com>
2021-08-31 14:41:45 +0800

    完成refresher的hook

--- lib/basic/views/refresher/refresher.dart
+++ lib/basic/views/refresher/refresher.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+
 import '../placeholder/loading_placeholder.dart';
 import '../placeholder/no_data_placeholder.dart';
-
 import 'normal_refresh_footer.dart';
 import 'normal_refresh_header.dart';
 import 'refresh_status.dart';
@@ -10,21 +10,49 @@ import 'refresh_status.dart';
 export 'package:flutter_easyrefresh/easy_refresh.dart';
 
 class Refresher extends StatelessWidget {
+  /// 子控件
   final Widget child;
+
+  /// 刷新控制器
   final EasyRefreshController controller;
+
+  /// 子控件当前刷新状态
   final RefreshStatus status;
+
+  /// 数据源是否为空
   final bool isListEmpty;
+
+  /// 是否还有更多数据
   final bool noMore;
+
+  /// 滚动控制器
   final ScrollController? scrollController;
-  final bool firstRefresh;
+
+  /// 头部文字颜色
   final Color? headerTextColor;
+
+  /// 尾部文字颜色
   final Color? footerTextColor;
+
+  /// 头部控件
   final Header? header;
+
+  /// 尾部控件
   final Footer? footer;
-  final Widget? emptyWidget;
+
+  /// 各种刷新状态的占位图
+  final Widget? placeholder;
+
+  /// 下拉刷新回调
   final OnRefreshCallback? onRefresh;
+
+  /// 上拉加载更多回调
   final OnLoadCallback? onLoad;
+
+  /// 是否开启无限加载，开启时没有上拉再回弹的效果
   final bool? enableInfiniteLoad;
+
+  /// 尾部缩进
   final double bottomPadding;
 
   const Refresher({
@@ -35,45 +63,16 @@ class Refresher extends StatelessWidget {
     required this.isListEmpty,
     this.noMore = false,
     this.scrollController,
-    this.firstRefresh = false,
     this.headerTextColor,
     this.footerTextColor,
     this.header,
     this.footer,
-    this.emptyWidget,
+    this.placeholder,
     this.onRefresh,
     this.onLoad,
     this.enableInfiniteLoad,
     this.bottomPadding = 0.0,
   }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    _finishAnimate();
-
-    final header = this.header ?? NormalRefreshHedaer(textColor: headerTextColor);
-    final footer = this.footer ??
-        NormalRefreshFooter(
-          textColor: footerTextColor,
-          enableInfiniteLoad: enableInfiniteLoad,
-          padding: EdgeInsets.only(bottom: bottomPadding),
-        );
-    final emptyWidget = this.emptyWidget ?? _buildEmptyWidget();
-
-    return EasyRefresh(
-      firstRefresh: firstRefresh,
-      enableControlFinishLoad: true,
-      enableControlFinishRefresh: true,
-      controller: controller,
-      scrollController: scrollController,
-      header: header,
-      footer: footer,
-      emptyWidget: emptyWidget,
-      onRefresh: onRefresh,
-      onLoad: onLoad,
-      child: child,
-    );
-  }
 
   void _finishAnimate() {
     switch (status) {
@@ -102,7 +101,7 @@ class Refresher extends StatelessWidget {
     }
   }
 
-  Widget? _buildEmptyWidget() {
+  Widget? _buildPlaceholderWidget() {
     switch (status) {
       case RefreshStatus.initial:
         return const LoadingPlaceholder();
@@ -114,9 +113,38 @@ class Refresher extends StatelessWidget {
         return null;
       case RefreshStatus.refreshFailure:
         controller.finishRefresh(success: false);
-        return const NoDataPlaceholder();
+        // return const NoDataPlaceholder();
+        return null;
       default:
         return null;
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _finishAnimate();
+
+    final header =
+        this.header ?? NormalRefreshHedaer(textColor: headerTextColor);
+    final footer = this.footer ??
+        NormalRefreshFooter(
+          textColor: footerTextColor,
+          enableInfiniteLoad: enableInfiniteLoad,
+          padding: EdgeInsets.only(bottom: bottomPadding),
+        );
+    final placeholder = this.placeholder ?? _buildPlaceholderWidget();
+
+    return EasyRefresh(
+      enableControlFinishLoad: true,
+      enableControlFinishRefresh: true,
+      controller: controller,
+      scrollController: scrollController,
+      header: header,
+      footer: footer,
+      emptyWidget: placeholder,
+      onRefresh: onRefresh,
+      onLoad: onLoad,
+      child: child,
+    );
   }
 }
--- lib/business/hook_demo/hook_page.dart
+++ lib/business/hook_demo/hook_page.dart
@@ -4,95 +4,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-import '../../basic/basic_instances.dart';
+import '../../basic/hooks/refresh_hook.dart';
 import '../../basic/views/base/base_app_bar.dart';
-import '../../basic/views/refresher/refresh_status.dart';
 import '../../basic/views/refresher/refresher.dart';
 import '../../common/views/title_action_item.dart';
 import '../cubit_demo/apis/cubit_apis.dart';
 import '../cubit_demo/models/post_entity.dart';
 
-class HookPage extends HookWidget {
-  const HookPage({Key? key}) : super(key: key);
+class NativeHookPage extends HookWidget {
+  const NativeHookPage({Key? key}) : super(key: key);
 
   Future<List<PostEntity>> _requestPsots(int start) {
     return CubitApis.getPosts(start);
   }
 
-  Widget _buildListView(List<PostEntity> posts) {
-    return ListView.builder(
-      itemCount: posts.length,
-      itemBuilder: (context, index) {
-        return TitleActionItem(title: posts[index].title);
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final refreshHook = useRefreshHook<PostEntity>();
-    logger.d('build');
+    final refreshHook = useRefreshHook<PostEntity>(request: _requestPsots);
     return Scaffold(
       appBar: BaseAppBar(
-        titleText: 'Cubit Demo',
+        titleText: 'Native Hook Refresh',
       ),
       body: Refresher(
-        controller: refreshHook.value.controller,
-        status: refreshHook.value.status,
-        isListEmpty: refreshHook.value.values.isEmpty,
-        // noMore: state.noMore,
-        // onRefresh: () async => cubit.refresh(),
-        // onLoad: () async => cubit.loadMore(),
-        child: _buildListView(refreshHook.value.values),
+        controller: refreshHook.controller,
+        status: refreshHook.status,
+        isListEmpty: refreshHook.values.isEmpty,
+        noMore: refreshHook.noMore,
+        onRefresh: () async => refreshHook.refresh(),
+        onLoad: () async => refreshHook.loadMore(),
+        child: ListView.builder(
+          itemCount: refreshHook.values.length,
+          itemBuilder: (context, index) {
+            return TitleActionItem(title: refreshHook.values[index].title);
+          },
+        ),
       ),
     );
-  }
-}
-
-class RefreshHookEntity<T> {
-  final EasyRefreshController controller;
-  final List<T> values;
-  final RefreshStatus status;
-
-  RefreshHookEntity(this.controller, this.values, this.status);
-}
-
-ValueNotifier<RefreshHookEntity<T>> useRefreshHook<T>() {
-  final hook = use(_RefreshHook<T>());
-  return useState(hook);
-}
-
-class _RefreshHook<T> extends Hook<RefreshHookEntity<T>> {
-  const _RefreshHook();
-
-  @override
-  _RefreshHookState<T> createState() => _RefreshHookState<T>();
-}
-
-class _RefreshHookState<E>
-    extends HookState<RefreshHookEntity<E>, _RefreshHook<E>> {
-  final _refreshController = EasyRefreshController();
-  var values = <E>[];
-  var status = RefreshStatus.initial;
-
-  @override
-  RefreshHookEntity<E> build(BuildContext context) {
-    return RefreshHookEntity(_refreshController, values, status);
-  }
-
-  @override
-  void dispose() {
-    _refreshController.dispose();
-    super.dispose();
-  }
-
-  void refresh(Future<List<E>> future) {
-    future.then((value) {
-      values = value;
-      status = RefreshStatus.refreshSuccess;
-    }).catchError((error) {
-      status = RefreshStatus.refreshFailure;
-    });
-    setState(() {});
   }
 }
