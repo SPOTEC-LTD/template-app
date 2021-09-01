085af2543af50a122d797e8efb100e4639cd9d04
Natai <liuleicumt@163.com>
2021-09-01 19:04:07 +0800

    FlutterHooks和Bloc结合的初步尝试

---
+++ lib/basic/views/base/base_stless_widget.dart
@@ -0,0 +1,9 @@
+// Author: Dean.Liu
+// DateTime: 2021/09/01 18:01
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+abstract class BaseStlessWidget extends HookWidget {
+  const BaseStlessWidget({Key? key}) : super(key: key);
+}
